d18c3e95106eade7c474010de7c52fb78ac8271c
Nur Aisyah Wulandari <60900121032@uin-alauddin.ac.id>
2024-01-21 11:22:05 +0800

    menambah dan memperbaiki isi VideoFragment.kt dan VideoViewModel.kt

---
+++ app/src/main/java/com/example/youtubeapikotlin/ui/video/VideoFragment.kt
@@ -0,0 +1,64 @@
+package com.example.youtubeapikotlin.ui.video
+
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class VideoFragment {
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private var isLoading = false
+private var isScroll = false
+private var currentItem = -1
+private var totalItem = -1
+private var scrollOutItem = -1
+private var isAllVideoLoaded = false
+
+
+
+
+ val manager = LinearLayoutManager(requireContext())
+
+        binding.rvVideo.LayoutManager = manager
+
+        binding.rvVideo.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState, newstate: Int){
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newstate = AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                    isScroll = true
+                }
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int){
+                super.onScrolled(recyclerView, dx, dy)
+                currentItem = manager.childCount
+                totalItem = manager.itemCount
+                scrollOutItem = manager.findFirstVisibleItemPosition()
+                if (isScroll && (currentItem + scrollOutItem == totalItem)){
+                    isScroll = false
+                    if (!isLoading){
+                        if (!isAllVideoLoaded){
+
+                        }else{
+                            Toast.makeText(requireContext(), "All video loaded", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        })
---
+++ app/src/main/java/com/example/youtubeapikotlin/ui/video/VideoViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.youtubeapikotlin.ui.video
+
+class VideoViewModel {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+private var nextPageToken: String? = null
+
+init{
+    getVideoList(false)
+}
+
+
+
+    fun getVideoList(isLoadMore: Boolean){
+
+    }
+
+}
+
