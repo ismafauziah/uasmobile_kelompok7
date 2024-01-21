6b14b1435a58fa6fa170e173f86b8e72a35db7b0
Nur Aisyah Wulandari <60900121032@uin-alauddin.ac.id>
2024-01-21 11:43:26 +0800

    menambahkan value isAllVideoLoaded pada VideoViewModel.kt

--- app/src/main/java/com/example/youtubeapikotlin/ui/video/VideoViewModel.kt
+++ app/src/main/java/com/example/youtubeapikotlin/ui/video/VideoViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.youtubeapikotlin.ui.video
 
+import androidx.lifecycle.MutableLiveData
+
 class VideoViewModel {
 
 
@@ -14,21 +16,33 @@ class VideoViewModel {
 
 
 
-
-
+private val _isAllVideoLoaded = MutableLiveData<Boolean>()
+val isAllVideoLoaded = _isAllVideoLoaded
 
 
 private var nextPageToken: String? = null
 
 init{
-    getVideoList(false)
+    getVideoList()
+}
+
+fun getVideoList(){
 }
 
 
 
-    fun getVideoList(isLoadMore: Boolean){
 
+
+
+    if (data != null){
+        if (data.nextPageToken != null){
+            nextPageToken = data.nextPageToken
+        } else {
+            _isAllVideoLoaded.value = true
+        }
+        if (data.items.isNotEmpty()){
+            _video.value = data
+        }
     }
-
 }
 
